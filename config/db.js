8e3519746916babcfd4add76578849964eed16e9
Ljl <jiale.lu@yuannian.tech>
2024-04-07 16:52:16 +0800

    修改sql配置

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
 const NODE_ENV = process.env.NODE_ENV.trim();
 let db = {};
 
-if (NODE_ENV === "development") {
+if (NODE_ENV === 'development') {
   db = {
-    database: "blogs",
-    user: "root",
-    password: "root",
-    port: "3306",
-    host: "122.51.53.84",
+    database: 'blogs',
+    user: 'root',
+    password: 'ljl990907',
+    port: '3306',
+    host: '122.51.53.84'
   };
-} else if (NODE_ENV === "production") {
+} else if (NODE_ENV === 'production') {
   db = {
-    database: "blogs",
-    user: "root",
-    password: "root",
-    port: "3306",
-    host: "122.51.53.84",
+    database: 'blogs',
+    user: 'root',
+    password: 'ljl990907',
+    port: '3306',
+    host: '122.51.53.84'
   };
 }
 module.exports = { db };
