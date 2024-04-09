294a28a10cbc2ab9e798453c84be1d1f9e436074
Ljl <jiale.lu@yuannian.tech>
2024-04-09 15:21:49 +0800

    修改环境变量配置

--- config/db.js
+++ config/db.js
@@ -1,21 +1,27 @@
-const NODE_ENV = process.env.NODE_ENV.trim();
+// const NODE_ENV = process.env.NODE_ENV.trim();
 let db = {};
-
-if (NODE_ENV === 'development') {
-  db = {
-    database: 'blogs',
-    user: 'root',
-    password: 'ljl990907',
-    port: '3306',
-    host: '122.51.53.84'
-  };
-} else if (NODE_ENV === 'production') {
-  db = {
-    database: 'blogs',
-    user: 'root',
-    password: 'ljl990907',
-    port: '3306',
-    host: '122.51.53.84'
-  };
-}
+db = {
+  database: 'blogs',
+  user: 'root',
+  password: 'ljl990907',
+  port: '3306',
+  host: '122.51.53.84'
+};
+// if (NODE_ENV === 'development') {
+//   db = {
+//     database: 'blogs',
+//     user: 'root',
+//     password: 'ljl990907',
+//     port: '3306',
+//     host: '122.51.53.84'
+//   };
+// } else if (NODE_ENV === 'production') {
+//   db = {
+//     database: 'blogs',
+//     user: 'root',
+//     password: 'ljl990907',
+//     port: '3306',
+//     host: '122.51.53.84'
+//   };
+// }
 module.exports = { db };
